9c3b487bf64ef34bd9baadc566479fd05c1b31fa
DASUCCESS <55310950+DASUCCESS@users.noreply.github.com>
2021-01-06 11:48:49 -0800

    Update formvalidation.js

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -2,13 +2,20 @@ const username = document.getElementById('username')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
 const btn = document.getElementById('botton')
-const msg = document.getElementById('message')
+const msg = document.getElementById('errormsg')
 
 
 form.addEventListener('submit', (e)=>{
-    let
-    if (username.Value.length <= 6){
-        msg.innerText = 'Your password is less than 6'
+    if(username.value === '' ){
+        msg.innerHTML = '<p> Input your name </p>'
+        msg.style.color = 'red'
+        
+        // msg.classList.add("danger")
+    }
+    else if (password.value === '' ){
+        msg.innerHTML = '<p> Input your password </p>'
+        msg.style.color = 'red'
+        // msg.classList.add("danger")
     }
     e.preventDefault()
 })

d4cc04d6ba34b703c7b9f9655803aef76e48a96f
dasuccess <dasuccessblogger@gmail.com>
2021-01-06 08:43:39 -0800

    javascript updated

--- js/formvalidation.js
+++ js/formvalidation.js
@@ -1,28 +1,20 @@
-const name = document.getElementById('name')
+const username = document.getElementById('username')
 const password = document.getElementById('password')
 const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const btn = document.getElementById('botton')
+const msg = document.getElementById('errormsg')
 
-form.addEventListener('submit', (e) => {
-  let messages = []
-  if (name.value === '' || name.value == null) {
-    messages.push('Name is required')
-  }
 
-  if (password.value.length <= 6) {
-    messages.push('Password must be longer than 6 characters')
-  }
-
-  if (password.value.length >= 20) {
-    messages.push('Password must be less than 20 characters')
-  }
-
-  if (password.value === 'password') {
-    messages.push('Password cannot be password')
-  }
-
-  if (messages.length > 0) {
+form.addEventListener('submit', (e)=>{
+    if(username.value === '' ){
+        msg.innerHTML = '<p> Input your name </p>'
+        msg.style.color = 'red'
+        // msg.classList.add("danger")
+    }
+    else if (password.value === '' ){
+        msg.innerHTML = '<p> Input your password </p>'
+        msg.style.color = 'red'
+        // msg.classList.add("danger")
+    }
     e.preventDefault()
-    errorElement.innerText = messages.join(', ')
-  }
 })
